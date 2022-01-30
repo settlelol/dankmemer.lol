cdd4acd0ae413612b2ee959bd2c3e3dea95814af
InBlue <InBlueOfficial@Outlook.com>
2022-01-30 18:59:05 +1000

    Fix page content positioning for new control panel

--- src/pages/control/store.tsx
+++ src/pages/control/store.tsx
@@ -10,11 +10,8 @@ import { withSession } from "../../util/session";
 export default function ControlStorePage({ user }: PageProps) {
 	return (
 		<Container title="Control" user={user}>
-			<div className="mx-8 xl:mx-0">
-				<div className="flex flex-col my-20">
-					<div className="flex flex-col space-y-4">
-						<GoBack />
-					</div>
+			<div className="mx-8">
+				<div className="flex flex-col my-10">
 					<div className="font-bold font-montserrat text-3xl text-dank-300 dark:text-light-100">
 						Manage the Store
 					</div>
