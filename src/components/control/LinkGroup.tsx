2de798d0a4e95ec14440a45fc9ad8cfa0d14ed8c
InBlue <InBlueOfficial@Outlook.com>
2022-01-30 16:01:37 +1000

    Control panel navbar
    With this navbar also comes the addition of the '@iconify/react' library, this will probably be used more than the material-icons font in the future as iconify includes much more and includes material icons.

---
+++ src/components/control/LinkGroup.tsx
@@ -0,0 +1,39 @@
+import clsx from "clsx";
+import { ReactNode, useState } from "react";
+
+interface Props {
+	title: string;
+	children?: ReactNode;
+}
+
+export default function LinkGroup({ title, children }: Props) {
+	const [collapsed, setCollapsed] = useState(false);
+
+	return (
+		<div className="mb-4">
+			<div
+				className="flex items-center cursor-pointer select-none"
+				onClick={() => setCollapsed(!collapsed)}
+			>
+				<p className="font-inter font-semibold">{title}</p>
+				<div className="flex-none order-1 grow mx-2 ml-3 bg-[#6E756F] h-[2px]"></div>
+				<span
+					className={clsx(
+						"order-2 material-icons text-[#6E756F] transition-transform",
+						collapsed ? "" : "rotate-180"
+					)}
+				>
+					expand_more
+				</span>
+			</div>
+			<div
+				className={clsx(
+					"flex justify-start flex-col mt-3 overflow-hidden transition-all duration-300",
+					collapsed ? "max-h-0" : "max-h-[100vh]"
+				)}
+			>
+				{children}
+			</div>
+		</div>
+	);
+}
