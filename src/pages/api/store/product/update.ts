8921b3909840a323e6a9cafe73da8de355d658c5
InBlue <InBlueOfficial@Outlook.com>
2022-01-31 18:12:24 +1000

    Manage products page (wip)
    The current page that manages store products will work, however I would like to make it look better or just redesign the page. Right now it works fine though.

---
+++ src/pages/api/store/product/update.ts
@@ -0,0 +1,66 @@
+import { Db } from "mongodb";
+import { NextApiResponse } from "next";
+import { dbConnect } from "src/util/mongodb";
+import { NextIronRequest, withSession } from "src/util/session";
+import { stripeConnect } from "src/util/stripe";
+import Stripe from "stripe";
+
+const handler = async (req: NextIronRequest, res: NextApiResponse) => {
+	if (req.method?.toLowerCase() !== "put")
+		return res.status(405).json({
+			error: `Method '${req.method?.toUpperCase()}' cannot be used on this endpoint.`,
+		});
+
+	const user = req.session.get("user");
+
+	if (!user) return res.status(401).json({ error: "You are not logged in." });
+	if (!user.developer)
+		return res.status(401).json({ error: "You can't do this." });
+
+	if (!req.query.productId)
+		return res
+			.status(400)
+			.json({ error: "No product id was provided in the request." });
+	if (!req.body)
+		return res.status(400).json({ error: "No body data was provided." });
+	if (!req.body.primaryTitle && !req.body.primaryBody)
+		return res
+			.status(400)
+			.json({ error: "Missing required fields in body." });
+
+	const productId = req.query.productId.toString();
+	try {
+		const db: Db = await dbConnect();
+		const stripe: Stripe = stripeConnect();
+
+		await db.collection("products").updateOne(
+			{ _id: productId },
+			{
+				$set: {
+					primaryTitle: req.body.primaryTitle.toString(),
+					primaryBody: req.body.primaryBody.toString(),
+					secondaryTitle: req.body.secondaryTitle.toString(),
+					secondaryBody: req.body.secondaryBody.toString(),
+				},
+			},
+			{ upsert: true }
+		);
+
+		const updatedProduct = await stripe.products.update(productId, {
+			metadata: {
+				lastUpdated: new Date().getTime(),
+			},
+		});
+		return res.status(200).json({
+			message: `Product '${productId}' was updated successfully.`,
+		});
+	} catch (e: any) {
+		console.error(e.message.replace(/"/, ""));
+		return res.status(500).json({
+			error: `Something went wrong while updating the product information '${productId}'`,
+			errorMessage: e.message.replace(/"/, ""),
+		});
+	}
+};
+
+export default withSession(handler);
