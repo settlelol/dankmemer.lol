d37314db474c0ae06df7bc9044c42e8ee2d2dcbe
InBlue <InBlueOfficial@Outlook.com>
2022-02-01 01:13:43 +1000

    Basic cart page, just cart items and quantity

---
+++ src/pages/store/cart.tsx
@@ -0,0 +1,67 @@
+import axios from "axios";
+import { GetServerSideProps } from "next";
+import { useEffect, useState } from "react";
+import { Title } from "src/components/Title";
+import Container from "src/components/ui/Container";
+import GoBack from "src/components/ui/GoBack";
+import { PageProps } from "src/types";
+import { authenticatedRoute } from "src/util/redirects";
+import { withSession } from "src/util/session";
+import { CartItem as CartItems } from ".";
+import CartItem from "src/components/store/cart/CartItem";
+
+export default function Cart({ user }: PageProps) {
+	const [cart, setCart] = useState<CartItems[]>([]);
+
+	useEffect(() => {
+		axios("/api/store/cart/get").then(({ data }) => {
+			setCart(data.cart);
+		});
+	}, []);
+
+	useEffect(() => {
+		console.log(cart);
+		axios({
+			url: "/api/store/cart/set",
+			method: "PUT",
+			data: { cartData: cart },
+		});
+	}, [cart]);
+
+	const updateQuantity = (index: number, quantity: number) => {
+		const _cart = [...cart];
+		_cart[index].quantity = quantity;
+		setCart(_cart);
+	};
+
+	const changeInterval = (index: number, interval: "month" | "year") => {
+		const _cart = [...cart];
+		_cart[index].price.interval = interval;
+		setCart(_cart);
+	};
+
+	return (
+		<Container title="Shopping Cart" user={user}>
+			<GoBack />
+			<div className="flex flex-col sm:flex-row justify-between items-center mt-12 space-y-2 sm:space-y-0">
+				<Title size="big">Shopping cart</Title>
+			</div>
+			<div className="mt-5 px-4 py-3 w-9/12 dark:bg-dark-200 rounded-lg">
+				<Title size="small">Your items</Title>
+				<div className="mt-2">
+					{cart.map((item, i) => (
+						<CartItem
+							index={i}
+							{...item}
+							updateQuantity={updateQuantity}
+							changeInterval={changeInterval}
+						/>
+					))}
+				</div>
+			</div>
+		</Container>
+	);
+}
+
+export const getServerSideProps: GetServerSideProps =
+	withSession(authenticatedRoute);
