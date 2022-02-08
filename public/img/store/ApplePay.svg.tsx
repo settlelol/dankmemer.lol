2561e9db79bdf81d82008591b364c2ad254689a2
InBlue <InBlueOfficial@Outlook.com>
2022-02-08 16:35:08 +1000

    Theoretically, apple and google pay should work

--- public/img/store/ApplePay.svg.tsx
+++ public/img/store/ApplePay.svg.tsx
@@ -2,7 +2,7 @@ export default function ApplyPay({ ...props }) {
 	return (
 		<svg
 			width="26"
-			height="10"
+			height="20"
 			viewBox="0 0 26 10"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
