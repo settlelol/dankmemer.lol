871f688e24805441bb93dec2bc4d72f4b3b99b6d
InBlue <InBlueOfficial@Outlook.com>
2022-02-09 16:57:13 +1000

    Move account information from checkoutform to its own component

--- src/components/store/checkout/CheckoutForm.tsx
+++ src/components/store/checkout/CheckoutForm.tsx
@@ -14,26 +14,22 @@ import {
 	StripeCardNumberElementChangeEvent,
 } from "@stripe/stripe-js";
 import axios from "axios";
-import clsx from "clsx";
 import Amex from "public/img/store/cards/Amex.svg";
 import Discover from "public/img/store/cards/Discover.svg";
 import Mastercard from "public/img/store/cards/Mastercard.svg";
 import Visa from "public/img/store/cards/Visa.svg";
 import { useEffect, useState } from "react";
 import { Title } from "src/components/Title";
-import Button from "src/components/ui/Button";
 import { CartItem } from "src/pages/store";
 import { DiscountItem } from "src/pages/store/checkout";
 import Input from "../Input";
 import PaymentOption from "./PaymentOption";
-import Link from "next/link";
 
-import { Icon as Iconify } from "@iconify/react";
 import router from "next/router";
 import Checkbox from "src/components/ui/Checkbox";
 import ApplyPay from "public/img/store/ApplePay.svg";
-import { useTheme } from "next-themes";
 import PaymentMethods from "./PaymentMethods";
+import AccountInformation from "./AccountInformation";
 
 interface Props {
 	clientSecret: string;
@@ -70,11 +66,11 @@ export default function CheckoutForm({
 	subtotalCost,
 	cart,
 }: Props) {
-	const { theme } = useTheme();
-
 	const [totalCost, setTotalCost] = useState<string>("0.00");
 	const [processingPayment, setProcessingPayment] = useState(false);
-	const [selectedPaymentOption, setSelectedPaymentOption] = useState("Card");
+	const [selectedPaymentOption, setSelectedPaymentOption] = useState<
+		"Card" | "PayPal" | "ApplePay" | "GooglePay" | "MicrosoftPay"
+	>("Card");
 	const [acceptsIntegratedWallet, setAcceptsIntegratedWallet] =
 		useState(false);
 	const [integratedWalletType, setIntegratedWalletType] = useState<
@@ -108,7 +104,7 @@ export default function CheckoutForm({
 	const [appliedSavings, setAppliedSavings] = useState(0);
 	const [appliedDiscount, setAppliedDiscount] = useState(false);
 
-	const [giftReceipient, setGiftRecipient] = useState("");
+	const [giftRecipient, setGiftRecipient] = useState("");
 	const [purchaseIsGift, setPurchaseIsGift] = useState(false);
 	const [receiptEmail, setReceiptEmail] = useState(userEmail);
 	const [acceptedTerms, setAcceptedTerms] = useState(false);
@@ -223,7 +219,7 @@ export default function CheckoutForm({
 				data: {
 					customerName: nameOnCard,
 					isGift: purchaseIsGift,
-					giftFor: giftReceipient,
+					giftFor: giftRecipient,
 				},
 			})
 				.then(() => {
@@ -479,205 +475,27 @@ export default function CheckoutForm({
 							</div>
 						</div>
 					)}
-					<div className="min-h-[200px]">
-						<h3 className="font-montserrat text-base font-bold">
-							Account information
-						</h3>
-						<div className="">
-							<p className="text-sm dark:text-neutral-300">
-								This purchase is being made for
-							</p>
-							<div className="mt-2 flex flex-col justify-start phone:flex-row phone:items-center">
-								<div className="mr-4 flex cursor-pointer select-none text-sm">
-									<p
-										className={clsx(
-											!purchaseIsGift
-												? "bg-dank-300 text-white"
-												: "text-neutral-400 dark:bg-black/30",
-											"rounded-l-md border-[1px] border-transparent px-3 py-1"
-										)}
-										onClick={() => setPurchaseIsGift(false)}
-									>
-										Myself
-									</p>
-									<p
-										className={clsx(
-											purchaseIsGift
-												? "bg-dank-300 text-white"
-												: "text-neutral-400 dark:bg-black/30",
-											"rounded-r-md border-[1px] border-transparent px-3 py-1"
-										)}
-										onClick={() => setPurchaseIsGift(true)}
-									>
-										Someone else
-									</p>
-								</div>
-								{purchaseIsGift && (
-									<div className="mt-2 phone:mt-0">
-										<Input
-											width="large"
-											type="text"
-											placeholder="270904126974590976"
-											className="!py-1 "
-											onChange={(e: any) =>
-												setGiftRecipient(e.target.value)
-											}
-										/>
-									</div>
-								)}
-							</div>
-						</div>
-						<div className="mt-3">
-							<p className="text-sm dark:text-[#7F847F]">
-								The following email will receive the purchase
-								receipt once the payment has been processed.
-							</p>
-							<Input
-								width="w-60"
-								type="text"
-								placeholder="admin@dankmemer.gg"
-								defaultValue={receiptEmail}
-								onChange={(e: any) =>
-									setReceiptEmail(e.target.value)
-								}
-								className="mt-2 !py-1"
-							/>
-							<Checkbox
-								state={acceptedTerms}
-								callback={() =>
-									setAcceptedTerms(!acceptedTerms)
-								}
-							>
-								I agree to Dank Memer's{" "}
-								<Link href="/terms">
-									<a className="text-dank-300 underline">
-										Terms of Serivce
-									</a>
-								</Link>{" "}
-								and{" "}
-								<Link href="/refunds">
-									<a className="text-dank-300 underline">
-										Refund Policy
-									</a>
-								</Link>
-								.
-							</Checkbox>
-							{acceptsIntegratedWallet &&
-							integratedWallet !== null ? (
-								<div className="mt-3">
-									<PaymentRequestButtonElement
-										options={{
-											paymentRequest: integratedWallet,
-											style: {
-												paymentRequestButton: {
-													type:
-														cart[0].selectedPrice
-															.type ===
-														"recurring"
-															? "subscribe"
-															: "check-out",
-													theme:
-														theme === "dark"
-															? "dark"
-															: "light",
-												},
-											},
-										}}
-									/>
-								</div>
-							) : (
-								<Button
-									size="medium-large"
-									className={clsx(
-										"mt-3 w-full",
-										!canCheckout
-											? "bg-neutral-500 text-neutral-800"
-											: ""
-									)}
-									disabled={!canCheckout}
-									onClick={confirmPayment}
-								>
-									{processingPayment ? (
-										<p className="flex">
-											<span className="mr-3">
-												<svg
-													width="23"
-													height="23"
-													viewBox="0 0 38 38"
-													xmlns="http://www.w3.org/2000/svg"
-												>
-													<defs>
-														<linearGradient
-															x1="8.042%"
-															y1="0%"
-															x2="65.682%"
-															y2="23.865%"
-															id="a"
-														>
-															<stop
-																stopColor="#fff"
-																stopOpacity="0"
-																offset="0%"
-															/>
-															<stop
-																stopColor="#fff"
-																stopOpacity=".631"
-																offset="63.146%"
-															/>
-															<stop
-																stopColor="#fff"
-																offset="100%"
-															/>
-														</linearGradient>
-													</defs>
-													<g
-														fill="none"
-														fill-rule="evenodd"
-													>
-														<g transform="translate(1 1)">
-															<path
-																d="M36 18c0-9.94-8.06-18-18-18"
-																id="Oval-2"
-																stroke="url(#a)"
-																strokeWidth="2"
-															>
-																<animateTransform
-																	attributeName="transform"
-																	type="rotate"
-																	from="0 18 18"
-																	to="360 18 18"
-																	dur="0.9s"
-																	repeatCount="indefinite"
-																/>
-															</path>
-															<circle
-																fill="#fff"
-																cx="36"
-																cy="18"
-																r="1"
-															>
-																<animateTransform
-																	attributeName="transform"
-																	type="rotate"
-																	from="0 18 18"
-																	to="360 18 18"
-																	dur="0.9s"
-																	repeatCount="indefinite"
-																/>
-															</circle>
-														</g>
-													</g>
-												</svg>
-											</span>
-											Processing payment...
-										</p>
-									) : (
-										<p>Pay with {selectedPaymentOption}</p>
-									)}
-								</Button>
-							)}
-						</div>
-					</div>
+					<AccountInformation
+						acceptsIntegratedWallet={acceptsIntegratedWallet}
+						integratedWallet={integratedWallet}
+						selectedPaymentOption={selectedPaymentOption}
+						acceptedTerms={acceptedTerms}
+						setAcceptedTerms={setAcceptedTerms}
+						isGift={purchaseIsGift}
+						setIsGift={setPurchaseIsGift}
+						giftRecipient={giftRecipient}
+						setGiftRecipient={setGiftRecipient}
+						receiptEmail={receiptEmail}
+						setReceiptEmail={setReceiptEmail}
+						processingPayment={processingPayment}
+						confirmPayment={confirmPayment}
+						canCheckout={canCheckout}
+						integratedWalletButtonType={
+							cart[0].selectedPrice.type === "recurring"
+								? "subscribe"
+								: "check-out"
+						}
+					/>
 				</div>
 			</div>
 		</div>
