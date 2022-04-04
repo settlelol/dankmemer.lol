1a9e9c43d9bc89644489a9e958d71f5ee0a4c8b5
InBlue <InBlueOfficial@Outlook.com>
2022-04-04 22:30:45 +1000

    Add sorting to the table
    I also moved the th elements from  the main page to a component that generates them all

---
+++ src/components/control/TableSortIcon.tsx
@@ -0,0 +1,47 @@
+import { Icon as Iconify } from "@iconify/react";
+import clsx from "clsx";
+
+export enum TableHeadersState {
+	TOP = 0,
+	BOTTOM = 1,
+}
+
+export namespace TableHeadersState {
+	export function opposite(_of: TableHeadersState) {
+		if (_of === TableHeadersState.TOP) {
+			return TableHeadersState.BOTTOM;
+		} else {
+			return TableHeadersState.TOP;
+		}
+	}
+}
+
+interface Props {
+	active: boolean;
+	current?: TableHeadersState;
+}
+
+export default function TableSortIcon({ active, current }: Props) {
+	return (
+		<div className="ml-3 grid place-items-center">
+			<Iconify
+				icon="carbon:chevron-sort-up"
+				className={clsx(
+					"absolute",
+					active && current === TableHeadersState.TOP
+						? "text-black dark:text-white"
+						: "dark:text-neutral-500"
+				)}
+			/>
+			<Iconify
+				icon="carbon:chevron-sort-down"
+				className={clsx(
+					"absolute",
+					active && current === TableHeadersState.BOTTOM
+						? "text-black dark:text-white"
+						: "dark:text-neutral-500"
+				)}
+			/>
+		</div>
+	);
+}
