7bbbfc132fa2a595f14fd63bb9c4aa7c2b609787
badosz0 <m@badosz.com>
2022-05-04 19:36:48 +0200

    rewrite navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 import clsx from "clsx";
-import Link from "next/link";
 import { useEffect, useState } from "react";
 import { User } from "../types";
 import { Avatar } from "./Avatar";
 import Button from "./ui/Button";
 import Dropdown from "./ui/Dropdown";
+import Link from "./ui/Link";
 
 interface Props {
 	user?: User;
@@ -44,7 +44,7 @@ export default function Navbar({ user }: Props) {
 	return (
 		<>
 			<div className="flex items-center justify-center text-lg">
-				<nav className="z-[1] mt-0 flex w-full max-w-7xl justify-between bg-light-200 p-4 drop-shadow-xl dark:bg-dark-200 dark:drop-shadow-none lg:mt-5 lg:w-11/12 lg:rounded-md">
+				<nav className="z-10 mt-0 flex w-full max-w-7xl justify-between bg-light-200 p-4 drop-shadow-xl dark:bg-dark-200 dark:drop-shadow-none lg:mt-5 lg:w-11/12 lg:rounded-md">
 					<div className="flex items-center">
 						<Link href="/">
 							<img
@@ -56,37 +56,20 @@ export default function Navbar({ user }: Props) {
 							/>
 						</Link>
 						<ul className="ml-5 hidden space-x-4 lg:flex">
-							<li className="inline-block text-gray-800 hover:text-dank-300 dark:text-light-200 dark:hover:text-dank-100">
-								<Link href="/commands">Commands</Link>
-							</li>
-							<li className="inline-block text-gray-800 hover:text-dank-300 dark:text-light-200 dark:hover:text-dank-100">
-								<Link href="/faq">FAQ</Link>
-							</li>
-							<li
-								className={clsx(
-									"inline-block  ",
-									discount
-										? "text-yellow-300 hover:text-yellow-400 dark:text-yellow-300 dark:hover:text-yellow-400"
-										: "text-gray-800 hover:text-dank-300 dark:text-light-200 dark:hover:text-dank-100"
-								)}
-							>
-								<Link href="/store">
-									<div className="flex cursor-pointer items-center space-x-2">
-										<span>Store</span>
-										{!!discount && (
-											<span className="rounded-md bg-yellow-300 px-2 font-montserrat text-xs font-bold text-dark-500">
-												SALE: {discount}%
-											</span>
-										)}
-									</div>
-								</Link>
-							</li>
-							<li className="inline-block text-gray-800 hover:text-dank-300 dark:text-light-200 dark:hover:text-dank-100">
-								<Link href="/items">Items</Link>
-							</li>
-							<li className="inline-block text-gray-800 hover:text-dank-300 dark:text-light-200 dark:hover:text-dank-100">
-								<Link href="/community">Community</Link>
-							</li>
+							<Link href="/commands">Commands</Link>
+							<Link href="/faq">FAQ</Link>
+							<Link href="/store">
+								<div className="flex cursor-pointer items-center space-x-2">
+									<span>Store</span>
+									{!!discount && (
+										<span className="rounded-md bg-yellow-300 px-2 font-montserrat text-xs font-bold text-dark-500">
+											SALE: {discount}%
+										</span>
+									)}
+								</div>
+							</Link>
+							<Link href="/items">Items</Link>
+							<Link href="/community">Community</Link>
 						</ul>
 						<div className="ml-4 inline-block font-montserrat text-xl font-bold text-gray-800 dark:text-light-200 lg:hidden">
 							Dank Memer
@@ -95,21 +78,15 @@ export default function Navbar({ user }: Props) {
 
 					<div className="relative hidden items-center lg:flex">
 						<Link href="https://discord.gg/dankmemerbot">
-							<a
-								className="inline-block text-gray-800 hover:text-dank-300 dark:text-light-200 dark:hover:text-dank-100"
-								rel="noreferrer noopener"
-							>
-								Support
-							</a>
+							Support
 						</Link>
 						{!user && (
-							<Link href="/api/auth/login">
-								<a
-									className="inline-block pl-4 text-dank-300"
-									rel="noreferrer noopener"
-								>
-									Login
-								</a>
+							<Link
+								className="pl-4"
+								href="/api/auth/login"
+								variant="primary"
+							>
+								Login
 							</Link>
 						)}
 						{user && (
@@ -193,31 +170,13 @@ export default function Navbar({ user }: Props) {
 				</nav>
 				{hamburger && (
 					<ul className="absolute top-[74px] z-[9999999] box-border flex h-screen w-screen flex-col bg-light-200 px-6 dark:bg-dark-200">
-						<Link href="/commands">
-							<li className="pt-5 text-dark-500 hover:text-light-600 dark:text-white">
-								Commands
-							</li>
-						</Link>
-						<Link href="/faq">
-							<li className="pt-5 text-dark-500 hover:text-light-600 dark:text-white">
-								Frequently asked questions
-							</li>
-						</Link>
-						<Link href="/community">
-							<li className="pt-5 text-dark-500 hover:text-light-600 dark:text-white">
-								Community
-							</li>
-						</Link>
-						<Link href="/store">
-							<li className="pt-5 text-dark-500 hover:text-light-600 dark:text-white">
-								Store
-							</li>
-						</Link>
-						<Link href="/items">
-							<li className="pt-5 text-dark-500 hover:text-light-600 dark:text-white">
-								Items
-							</li>
-						</Link>
+						<div className="flex flex-col space-y-8">
+							<Link href="/commands">Commands</Link>
+							<Link href="/faq">Frequently asked questions</Link>
+							<Link href="/community">Community</Link>
+							<Link href="/store">Store</Link>
+							<Link href="/items">Items</Link>
+						</div>
 						{user ? (
 							<div className="mt-5 border-t-[1px] border-dank-600 pt-5">
 								<div
@@ -257,39 +216,26 @@ export default function Navbar({ user }: Props) {
 								</div>
 								<div
 									id="account-links"
-									className="ease mb-5 overflow-hidden pl-3 transition-all"
-									style={{
-										height: mobileAccountExpanded
-											? user.moderator
-												? "194px"
-												: "146px"
-											: "0px",
-									}}
+									className={clsx(
+										"ease overflow-hiddens flex flex-col space-y-8 py-8 pl-3 transition-all",
+										mobileAccountExpanded
+											? "hidden"
+											: "inline-block"
+									)}
 								>
 									<Link href="/community/notifications">
-										<li className="pt-5 text-dark-400 hover:text-light-600 dark:text-white">
-											Notifications
-										</li>
+										Notifications
 									</Link>
-									<Link href="/appeals">
-										<li className="pt-5 text-dark-500 hover:text-light-600 dark:text-white">
-											Appeal a ban
-										</li>
-									</Link>
-									<Link href="/reports">
-										<li className="pt-5 text-dark-500 hover:text-light-600 dark:text-white">
-											Report a user
-										</li>
-									</Link>
+									<Link href="/appeals">Appeal a ban</Link>
+									<Link href="/reports">Report a user</Link>
 									{user?.moderator && (
 										<Link href="/control">
-											<li className="pt-5 text-dark-500 hover:text-light-600 dark:text-white">
-												Control panel
-											</li>
+											Control panel
 										</Link>
 									)}
 								</div>
 								<Button
+									className="mt-4"
 									variant="danger"
 									size="medium"
 									block
