e9888bf0c21b485e83aa58d851d4cfed6d912239
badosz0 <m@badosz.com>
2022-05-04 19:44:25 +0200

    secondary variant

--- src/components/ui/Link.tsx
+++ src/components/ui/Link.tsx
@@ -8,6 +8,8 @@ const variantClasses = {
 		"text-dank-300 hover:text-dank-200 dark:text-dank-300 dark:hover:text-dank-200",
 	regular:
 		"text-gray-800 hover:text-dank-300 dark:text-light-200 dark:hover:text-dank-100",
+	secondary:
+		"text-dark-100 hover:text-dank-200 dark:text-gray-300 dark:hover:text-dank-300",
 };
 
 interface Props {
