c56853e621a478153629996d2ed40ffed28b9423
InBlue <InBlueOfficial@Outlook.com>
2022-05-06 21:06:59 +1000

    Add Plans class to PayPal util so it is accessible

--- src/util/paypal/index.ts
+++ src/util/paypal/index.ts
@@ -2,6 +2,7 @@ import Products from "./classes/Products";
 import Webhooks from "./classes/Webhooks";
 import Simulations from "./classes/Simulations";
 import Orders from "./classes/Orders";
+import Plans from "./classes/Plans";
 
 if (!process.env.PAYPAL_CLIENT_ID && !process.env.PAYPAL_CLIENT_SECRET) {
 	throw new Error(
@@ -22,12 +23,14 @@ export enum AllowedMethods {
 
 export default class PayPal {
 	products: Products;
+	plans: Plans;
 	orders: Orders;
 	webhooks: Webhooks;
 	simulate: Simulations;
 
 	constructor() {
 		this.products = new Products();
+		this.plans = new Plans();
 		this.orders = new Orders();
 		this.webhooks = new Webhooks();
 		this.simulate = new Simulations();
