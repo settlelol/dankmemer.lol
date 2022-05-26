ab06291825df8c0505bf0eb27b420e0abbfab633
InBlue <InBlueOfficial@Outlook.com>
2022-05-26 20:28:47 +1000

    Add `image` field to purchase history

--- src/pages/api/customers/history.ts
+++ src/pages/api/customers/history.ts
@@ -1,9 +1,14 @@
 import { Db } from "mongodb";
 import { NextApiResponse } from "next";
 import { dbConnect } from "src/util/mongodb";
-import PayPal from "src/util/paypal";
 import { NextIronRequest, withSession } from "src/util/session";
 import { stripeConnect } from "src/util/stripe";
+import { PurchaseRecord } from "../store/checkout/finalize/paypal";
+
+interface AggregatedData {
+	discordId: string;
+	purchases: PurchaseRecord[];
+}
 
 const handler = async (req: NextIronRequest, res: NextApiResponse) => {
 	const user = req.session.get("user");
@@ -11,36 +16,43 @@ const handler = async (req: NextIronRequest, res: NextApiResponse) => {
 		return res.status(401).json({ error: "You are not logged in." });
 	}
 	const db: Db = await dbConnect();
-	const purchaseHistory = await db
-		.collection("customers")
-		.aggregate([
-			{
-				$match: {
-					discordId: req.query.id,
-				},
-			},
-			{
-				$lookup: {
-					from: "purchases",
-					localField: "purchases.id",
-					foreignField: "_id",
-					as: "data",
-				},
-			},
-			{
-				$project: {
-					discordId: req.query.id,
-					purchases: "$data",
-				},
-			},
-			{
-				$unset: ["_id"],
-			},
-		])
-		.toArray();
-
 	const stripe = stripeConnect();
-	const paypal = new PayPal();
+
+	const purchaseHistory = (
+		await db
+			.collection("customers")
+			.aggregate([
+				{
+					$match: {
+						discordId: req.query.id,
+					},
+				},
+				{
+					$lookup: {
+						from: "purchases",
+						localField: "purchases.id",
+						foreignField: "_id",
+						as: "data",
+					},
+				},
+				{
+					$project: {
+						discordId: req.query.id,
+						purchases: "$data",
+					},
+				},
+				{
+					$unset: ["_id"],
+				},
+			])
+			.toArray()
+	)[0] as AggregatedData;
+
+	for (let purchase of purchaseHistory.purchases) {
+		for (let item of purchase.items) {
+			item = Object.assign(item, { image: (await stripe.products.retrieve(item.id)).images[0] });
+		}
+	}
 
 	return res.status(200).json({ purchases: purchaseHistory });
 };
