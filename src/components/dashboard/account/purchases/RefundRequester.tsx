77ad1149eccff70b546718f96cd335a7134c1132
InBlue <InBlueOfficial@Outlook.com>
2022-06-08 22:14:09 +1000

    Dashboard light theme

--- src/components/dashboard/account/purchases/RefundRequester.tsx
+++ src/components/dashboard/account/purchases/RefundRequester.tsx
@@ -136,7 +136,7 @@ export default function DisputeCreator({ purchase, userId, close }: Props) {
 							className={clsx(
 								"flex items-center justify-between",
 								"rounded-md border-[1px] border-[#3C3C3C]",
-								"bg-light-500 transition-colors dark:bg-black/40 dark:text-neutral-400",
+								"bg-light-500 text-neutral-800 transition-colors dark:bg-black/40 dark:text-neutral-400",
 								"w-full px-3 py-2 text-sm"
 							)}
 						>
@@ -157,7 +157,7 @@ export default function DisputeCreator({ purchase, userId, close }: Props) {
 									className={clsx(
 										"flex items-center justify-between",
 										"rounded-md border-[1px] border-[#3C3C3C]",
-										"bg-light-500 transition-colors dark:bg-black/40 dark:text-neutral-400",
+										"bg-light-500 text-neutral-800 transition-colors dark:bg-black/40 dark:text-neutral-400",
 										"w-full px-3 py-2 text-sm"
 									)}
 								>
