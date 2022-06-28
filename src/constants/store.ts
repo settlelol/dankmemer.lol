f486b83c5568e73ffd9e8fa430bf86c4a99087ec
InBlue <InBlueOfficial@Outlook.com>
2022-06-28 13:28:02 +1000

    Change store constants
    Includes changes for blocked countries and minimum age for lootbox purchases

---
+++ src/constants/store.ts
@@ -0,0 +1,70 @@
+export const STORE_BLOCKED_COUNTRIES = [
+	"BE",
+	"NL",
+	"CU",
+	"VG",
+	"SD",
+	"SO",
+	"MR",
+	"LY",
+	"DZ",
+	"AE",
+	"TH",
+	"SY",
+	"SA",
+	"QA",
+	"PK",
+	"MV",
+	"ID",
+	"CN",
+	"BN",
+	"BH",
+	"MH",
+];
+
+export const STORE_NO_MIN_AGE = [
+	"AD",
+	"AF",
+	"AI",
+	"BA",
+	"BD",
+	"ER",
+	"ET",
+	"GA",
+	"GQ",
+	"GW",
+	"KH",
+	"MN",
+	"PG",
+	"SS",
+	"TN",
+];
+
+// Default assumed minimum age is 18. The following countries have a different minimum age.
+export const STORE_CUSTOM_MIN_AGE = {
+	KN: 21,
+	US: 21,
+	DO: 13,
+	CA: 19,
+	NG: 20,
+	NA: 21,
+	EG: 21,
+	CI: 21,
+	VN: 21,
+	TH: 21,
+	SG: 21,
+	PH: 21,
+	MY: 21,
+	MO: 21,
+	LB: 21,
+	JP: 20,
+	IN: 21,
+	AM: 21,
+	GR: 23,
+	EE: 21,
+	NZ: 20,
+};
+
+export const LOOT_MINIMUM_PURCHASE_VALUE = 3;
+export const LOOT_MINIMUM_DISCOUNT_VALUE = 20;
+export const LOOT_FLAT_DISCOUNT_PERCENTAGE = 10;
