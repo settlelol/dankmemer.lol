994acb7dad289b24aa3ae8e4eb63836a54274a49
InBlue <InBlueOfficial@Outlook.com>
2022-07-17 23:32:09 +1000

    Actually remove dialog element type

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -10,7 +10,7 @@ interface Props {
 }
 
 export default function Dialog({ open, onClose, children, closeButton }: Props) {
-	const dialog = useRef<HTMLDialogElement>(null);
+	const dialog = useRef<any>(null);
 	const state = useRef(false);
 
 	const closeDialog = () => {
@@ -20,7 +20,7 @@ export default function Dialog({ open, onClose, children, closeButton }: Props) 
 
 	const dismissDialog = ({ target }: MouseEvent) => {
 		// @ts-expect-error
-		if (target && target.nodeName === "DIALOG" && dialog.current) {
+		if (target && target.nodeName === "DIALOG") {
 			dialog.current?.close("dismiss");
 		}
 	};
