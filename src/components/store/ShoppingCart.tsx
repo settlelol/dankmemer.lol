25a6510aad7a857c55f1c43252106f16fdd51bf2
InBlue <InBlueOfficial@Outlook.com>
2022-07-18 12:15:36 +1000

    Remove arbitrary value

--- src/components/store/ShoppingCart.tsx
+++ src/components/store/ShoppingCart.tsx
@@ -62,8 +62,8 @@ export default function ShoppingCart({ totalCost, cart, setCart, label }: Props)
 			</Button>
 			{showCart &&
 				(cart.length >= 1 ? (
-					<div className="absolute right-0 pt-2 motion-safe:animate-slide-in">
-						<div className="w-[420px] rounded-md bg-neutral-200 py-3 px-4 dark:bg-dank-600">
+					<div className="absolute right-0 w-screen max-w-md pt-2 motion-safe:animate-slide-in">
+						<div className="w-full rounded-md bg-neutral-200 py-3 px-4 dark:bg-dank-600">
 							<Title size="small">Your cart</Title>
 							<div className="flex flex-col">
 								<div>
